e00941113e354b1d4c618f8923f517745b4766b5
williammango <williammango2015@gmail.com>
2019-06-30 22:45:28 +0300

    create regestarion route

--- app/user.py
+++ app/user.py
@@ -1,7 +1,9 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer,primary_key = True)
@@ -19,13 +21,19 @@ class User(db.Model):
     @password.setter
     def password(self, password):
         self.pass_secure = generate_password_hash(password)
+    
 
 
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
+    
 
 def __repr__(self):
     return f'User {self.username}', '{self.image}'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
     
 class Pitch(db.Model):
